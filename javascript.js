cd41aceaa2b6fec004bac0d676a5473d9449477f
CowboyAragorn <Jacobman0313@gmail.com>
2021-12-05 12:33:30 -0500

    Figured out how to call functions, working prototype!

--- javascript.js
+++ javascript.js
@@ -3,27 +3,46 @@ function computerPlay(){
     let computerSelection = Math.floor((Math.random()*3)+1);
     if (computerSelection === 1){
         computerSelection = "rock";
-        console.log(computerSelection);
+        return computerSelection;
     }
     else if (computerSelection === 2) {
         computerSelection = "paper";
-        console.log(computerSelection);
+        return computerSelection;
     }
     else {
         computerSelection = "scissors";
-        console.log(computerSelection);
+        return computerSelection;
     }
     //Returns that integer//
 }
 
 
 
-function playerSelection(){
+function playerPlay(){
     //Player types in rock, paper, or scissors after message to select//
     let playerSelection = prompt("Choose your fighter: rock, paper, or scissors?");
     //Convert the input to lowercase//
     playerSelection = playerSelection.toLowerCase();
     //Convert the strings to numbers with rock being 1, scissors being 2, and paper being 3//
-    console.log(playerSelection);
+    return playerSelection;
 }
 
+let computerPick = computerPlay();
+let playerPick = playerPlay();
+
+console.log("You pick " + playerPick);
+console.log("Computer picks " + computerPick);
+
+function playRound (playerPick, computerPick) {
+    if (playerPick === "rock" && computerPick === "rock"){
+        return "Rock ties with Rock - try again";
+    }
+    else if (playerPick ==="rock" && computerPick === "scissors") {
+        return "You win - Rock beats scissors!"
+    }
+    else if (playerPick ==="rock" && computerPick === "paper"){
+        return "You lose :( - Paper beats Rock";
+    }
+}
+
+console.log(playRound(playerPick, computerPick))
