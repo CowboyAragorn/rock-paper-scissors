f91e6aea8f16012d561daf2db4cd73d5022f306a
CowboyAragorn <Jacobman0313@gmail.com>
2021-12-05 11:39:17 -0500

    Cleaned up code, removed numbers

--- javascript.js
+++ javascript.js
@@ -1,32 +1,29 @@
 function computerPlay(){
     //Computer selects an integer between 1-3//
     let computerSelection = Math.floor((Math.random()*3)+1);
+    if (computerSelection === 1){
+        computerSelection = "rock";
+        console.log(computerSelection);
+    }
+    else if (computerSelection === 2) {
+        computerSelection = "paper";
+        console.log(computerSelection);
+    }
+    else {
+        computerSelection = "scissors";
+        console.log(computerSelection);
+    }
     //Returns that integer//
-    return(computerSelection);
 }
 
-function round(){
-//Player types in rock, paper, or scissors after message to select//
+
+
+function playerSelection(){
+    //Player types in rock, paper, or scissors after message to select//
     let playerSelection = prompt("Choose your fighter: rock, paper, or scissors?");
-//Convert the input to lowercase//
+    //Convert the input to lowercase//
     playerSelection = playerSelection.toLowerCase();
-//Convert the strings to numbers with rock being 1, scissors being 2, and paper being 3
-    let playerRock = 1
-    let playerPaper = 2
-    let playerScissors = 3
-    if (playerSelection = "rock") {
-        playerSelection = playerRock;
-        }
-    else if (playerSelection = "paper") {
-        playerSelection = playerPaper;
-        }
-    else (playerSelection = "scissors") 
-        playerSelection = playerScissors;
-        
-console.log(playerSelection);
-//play a single round comparing the player selection with computer selection//
-//and outputting victory/defeat messages//
-//Return results of function//
+    //Convert the strings to numbers with rock being 1, scissors being 2, and paper being 3//
+    console.log(playerSelection);
 }
 
-round();
